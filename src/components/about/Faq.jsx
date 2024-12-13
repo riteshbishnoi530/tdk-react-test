f5640973fcfeeedc53a5fb2e1a2a0c1a3e427511
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-13 14:59:05 +0530

    text size

--- src/components/about/Faq.jsx
+++ src/components/about/Faq.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Faq() {
   return (
     <div>
-        <h1 className='text-6xl text-red-600 font-bold text-center'>Coming Soon</h1>
+        <h1 className='text-6xl text-red-600 font-bold text-center max-sm:text-2xl'>Coming Soon</h1>
     </div>
   )
 }
